7f98c518000039760c9fd115250278173d3d270e
Jamshed Alam <jamshedalam1777@gmail.com>
2025-08-13 17:25:32 +0530

    added prisma and basic auth

---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,20 @@
+
+//Register a new user
+
+import { NextFunction, Request, Response } from "express";
+import { validateRegistrationData } from "../utils/auth.helper";
+import prisma from "../../../../packages/libs/prisma";
+import { ValidationError } from "../../../../packages/error-handler";
+
+export const userRegistration = async (req: Request, res: Response, next: NextFunction) => {
+    validateRegistrationData(req.body, "user");
+    const { name, email } = req.body;
+
+
+    const existingUser = await prisma.users.findUnique({ where: email });
+
+    if (existingUser) {
+        return next(new ValidationError("User already exists with this email"));
+    };
+    await
+}
